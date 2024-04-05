e7e18524216711ae59e6577a63d7991e759c6892
Tony Barquera <barqueratony@gmail.com>
2024-04-05 07:01:56 -0600

    [INITIAL] base project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-bold text-4xl text-white">Planificador de Gastos</h1>
+      </header>
+
+      <div className="max-w-4xl mx-5 lg:mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+
+      </div>
+    </>
+  )
+}
+
+export default App

b5ba5f4b225141a809d704ec7bfd6116417ca255
Tony Barquera <barqueratony@gmail.com>
2024-04-05 07:20:32 -0600

    [ADD] budget state in Budget

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,9 +1,17 @@
+import { ChangeEvent, useState } from "react";
+
 function BudgetForm() {
+  const [ budget, setBudget ] = useState(0);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBudget(+e.target.value);
+  }
+
   return (
     <form className="space-y-5">
       <div className="flex flex-col space-y-5">
         <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">Definir Presupuesto</label>
-        <input type="number" name="budget" id="budget" className="w-full bg-white border border-gray-200 p-2 rounded" placeholder="Define tu presupuesto"/>
+        <input type="number" name="budget" id="budget" className="w-full bg-white border border-gray-200 p-2 rounded" placeholder="Define tu presupuesto" value={budget} onChange={handleChange}/>
       </div>
 
       <input type="submit" value="Definir Presupuesto" className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-bold uppercase rounded-lg shadow-lg"/>

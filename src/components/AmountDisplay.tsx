13ed75ae9364c16ba400ee808818cffc0b193e4f
Tony Barquera <barqueratony@gmail.com>
2024-04-05 19:57:38 -0600

    [ADD] create AmountDisplay Component

---
+++ src/components/AmountDisplay.tsx
@@ -0,0 +1,15 @@
+type AmountDisplayProps = {
+  label: string;
+  amount: number;
+}
+
+function AmountDisplay({ label, amount } : AmountDisplayProps) {
+  return (
+    <p className="text-2xl text-blue-600 font-bold">
+      {label}: {' '}
+      <span className="font-black text-black">${amount}</span>
+    </p>
+  )
+}
+
+export default AmountDisplay;

15ec64ca5cf8653399e578192cd49afc64f6a4d3
Tony Barquera <barqueratony@gmail.com>
2024-04-05 19:07:23 -0600

    [ADD] budget validation

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState, useMemo } from "react";
 
 function BudgetForm() {
   const [ budget, setBudget ] = useState(0);
@@ -7,6 +7,10 @@ function BudgetForm() {
     setBudget(+e.target.value);
   }
 
+  const isValid = useMemo(() => {
+    return isNaN(budget) || budget <= 0;
+  },[budget]);
+
   return (
     <form className="space-y-5">
       <div className="flex flex-col space-y-5">
@@ -14,7 +18,7 @@ function BudgetForm() {
         <input type="number" name="budget" id="budget" className="w-full bg-white border border-gray-200 p-2 rounded" placeholder="Define tu presupuesto" value={budget} onChange={handleChange}/>
       </div>
 
-      <input type="submit" value="Definir Presupuesto" className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-bold uppercase rounded-lg shadow-lg"/>
+      <input type="submit" value="Definir Presupuesto" className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-bold uppercase rounded-lg shadow-lg disabled:opacity-40" disabled={isValid}/>
     </form>
   )
 }

bf04a8166213fbd6163eb8f401acea75c6a4a303
Tony Barquera <barqueratony@gmail.com>
2024-04-05 19:58:10 -0600

    [ADD] use AmountDisplay in BudgetTracker

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,7 +1,35 @@
+import AmountDisplay from "./AmountDisplay";
+
 function BudgetTracker() {
   return (
     <>
-      <h1>Budget Tracker</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="flex justify-center">
+          <img src="/grafico.jpg" alt="Gráfica de gastos" />
+        </div>
+
+        <div className="flex flex-col justify-center items-center gap-8">
+          <button
+            type="button"
+            className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+          >Reiniciar App</button>
+
+          <AmountDisplay 
+            label="Presupuesto"
+            amount={300}
+          />
+
+          <AmountDisplay 
+            label="Disponible"
+            amount={200}
+          />
+
+          <AmountDisplay 
+            label="Gastado"
+            amount={100}
+          />
+        </div>
+      </div>
     </>
   )
 }

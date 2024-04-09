1c3526db5eae85b51b7037f59c2aaf305660001c
Tony Barquera <barqueratony@gmail.com>
2024-04-09 07:05:54 -0600

    [ADD] localStorage budget and expenses

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import BudgetForm from "./components/BudgetForm";
 import BudgetTracker from "./components/BudgetTracker";
 import { useBudget } from "./hooks/useBudget";
@@ -9,6 +9,11 @@ function App() {
   const { state } = useBudget();
 
   const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
+
+  useEffect(() => {
+    localStorage.setItem('budget', state.budget.toString());
+    localStorage.setItem('expenses', JSON.stringify(state.expenses));
+  }, [state]);
 
   return (
     <>
